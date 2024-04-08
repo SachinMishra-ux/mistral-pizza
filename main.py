a909862751ab79754f79ecb63bca8b2e08caaea6
Sachin <sachin.mishra@datasociety.com>
2024-04-08 09:19:38 +0530

    changes made

--- main.py
+++ main.py
@@ -1,12 +1,57 @@
-from ingestion import get_index
+from llama_index.core import VectorStoreIndex,SimpleDirectoryReader,ServiceContext
+import torch
+
+from llama_index.llms.llama_cpp import LlamaCPP
+from llama_index.llms.llama_cpp.llama_utils import (
+    messages_to_prompt,
+    completion_to_prompt,
+)
+
+from langchain.embeddings import HuggingFaceEmbeddings
+from llama_index.embeddings.langchain import LangchainEmbedding
 from flask import Flask, request, jsonify
+from quart import Quart, request, jsonify
 from llama_index.core.memory import ChatMemoryBuffer
 
-# Initialize Flask application
-app = Flask(__name__)
+## create embeddings
 
-path = "/root/mistral-pizza/mistral-pizza/Data"
-index = get_index(path)
+documents = SimpleDirectoryReader('/Users/sachinmishra/Desktop/VoiceMLPizza/mistral-pizza/Data').load_data()
+
+llm = LlamaCPP(
+    # You can pass in the URL to a GGML model to download it automatically
+    model_url='https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-GGUF/resolve/main/mistral-7b-instruct-v0.1.Q4_K_M.gguf',
+    # optionally, you can set the path to a pre-downloaded model instead of model_url
+    model_path=None,
+    temperature=0.1,
+    max_new_tokens=256,
+    # llama2 has a context window of 4096 tokens, but we set it lower to allow for some wiggle room
+    context_window=3900,
+    # kwargs to pass to __call__()
+    generate_kwargs={},
+    # kwargs to pass to __init__()
+    # set to at least 1 to use GPU
+    model_kwargs={"n_gpu_layers": -1},
+    # transform inputs into Llama2 format
+    messages_to_prompt=messages_to_prompt,
+    completion_to_prompt=completion_to_prompt,
+    verbose=True,
+)
+
+
+embed_model = LangchainEmbedding(
+HuggingFaceEmbeddings(model_name="thenlper/gte-large")
+)
+
+service_context = ServiceContext.from_defaults(
+    chunk_size=256,
+    llm=llm,
+    embed_model=embed_model
+)
+
+index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+
+# Initialize Flask application
+app = Quart(__name__)
 
 # Initialize ChatMemoryBuffer
 memory = ChatMemoryBuffer.from_defaults(token_limit=1500)
@@ -32,6 +77,7 @@ def chat():
 
     # Generate response using chat engine
     response = chat_engine.chat(user_message)
+    print(response)
 
     # Return response as JSON
     return jsonify({'response': response})
